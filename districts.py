6509ce9186bec4565dcccd78577a3aa354972c7d
Menachem Franko <franko@campus.technion.ac.il>
2021-04-30 15:53:47 +0300

    2_def_districts

--- districts.py
+++ districts.py
@@ -1,3 +1,4 @@
+import pandas
 import data
 
 
@@ -7,5 +8,22 @@ class Districts:
 
     def filter_districts(self, letters):
         all_regions_list = self.dataset.get_all_districts()
+        relevant_regions_list = []
+        for region in all_regions_list:
+            if region[0] in letters:
+                relevant_regions_list.append(region)
+        self.dataset.set_districts_data(relevant_regions_list)
+
+    def print_details(self, features, statistic_functions):
+        for feature in features:
+            print(feature + ": ", end="")
+            for func in statistic_functions:
+                if func == statistic_functions[-1]:
+                    print(func(data[feature]))
+                else:
+                    print("{:.14f}".format(func(data[feature])) + ", ", end="")
+
+
+
 
 
