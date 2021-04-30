28c27e3f030a58fb07540c80367e116acf8834d7
Menachem Franko <franko@campus.technion.ac.il>
2021-04-30 16:22:53 +0300

    docstring

--- data.py
+++ data.py
@@ -4,16 +4,30 @@ import pandas
 class Data:
 
     def __init__(self, path):
+        """
+        initial the new data object
+        :param path: the path of the dictionary to work with
+        """
         df = pandas.read_csv(path)
         self.data = df.to_dict(orient="list")
 
     def get_all_districts(self):
+        """
+        this method return a list of the regions in the data object
+        :return: list of the regions in the dataset
+        """
         regions_set = {}
         for region in self.data["denominazione_region"]:
             regions_set.add(region)
         return list(regions_set)
 
     def set_districts_data(self, districts):
+        """
+        this method receives a list of districts and change the data object by remain only the values for
+        the regions which are in that list
+        :param districts: a list of regions
+        :return: void
+        """
         new_data = {}
         for i in self.data.keys():
             new_data.add(i)
--- districts.py
+++ districts.py
@@ -4,9 +4,19 @@ import data
 
 class Districts:
     def __init__(self, dataset):
+        """
+        initial the districts object by using data object
+        :param dataset: the dataset that we work with
+        """
         self.dataset = dataset
 
     def filter_districts(self, letters):
+        """
+        that method receives a list of letters and change the object according that list and the region names
+        in the object
+        :param letters: a list of letters to convert the object by
+        :return: void
+        """
         all_regions_list = self.dataset.get_all_districts()
         relevant_regions_list = []
         for region in all_regions_list:
@@ -15,6 +25,13 @@ class Districts:
         self.dataset.set_districts_data(relevant_regions_list)
 
     def print_details(self, features, statistic_functions):
+        """
+        that method receives lists of features and statistic_funcs and print the results of the funcs according
+        the features which are keys in the object
+        :param features: a list of features to print stats for
+        :param statistic_functions: a list of statistic funcs
+        :return: void
+        """
         for feature in features:
             print(feature + ": ", end="")
             for func in statistic_functions:
