5f64c130d7192a31c71426182edc7622c96282b2
Menachem Franko <franko@campus.technion.ac.il>
2021-04-30 10:27:59 +0300

    first

--- data.py
+++ data.py
@@ -7,3 +7,11 @@ class Data:
         df = pandas.read_csv(path)
         self.data = df.to_dict(orient="list")
 
+    def get_all_districts(self):
+        regions_set = {}
+        for region in self.data["denominazione_region"]:
+            regions_set.add(region)
+        return list(regions_set)
+
+    def set_districts_data(self, districts):
+
