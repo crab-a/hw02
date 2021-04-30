63ebfa000a0e6565a2c2c3ee52b409dfa947888c
Menachem Franko <franko@campus.technion.ac.il>
2021-04-30 11:02:09 +0300

    begin_districts

--- districts.py
+++ districts.py
@@ -1,5 +1,11 @@
 import data
+
+
 class Districts:
     def __init__(self, dataset):
-        self.data = dataset
+        self.dataset = dataset
+
+    def filter_districts(self, letters):
+        all_regions_list = self.dataset.get_all_districts()
+
 
