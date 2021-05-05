0ea5869ba9a84c8fe714521ace9e86ed20d66ab2
crab-a <crab@notmailinator.com>
2021-05-05 19:24:48 +0300

    captial N

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main(argv):
     #  q1
     print('Question 1:')
 
-    the_data_q1 = data.Data(argv[1])  # "dpc-covid19-ita-regioni_sample.csv"
+    the_data_q1 = data.Data(argv[1])
     dist1 = districts.Districts(the_data_q1)
 
     features = ['hospitalized_with_symptoms', 'intensive_care', 'total_hospitalized', 'home_insulation']
@@ -34,7 +34,7 @@ def main(argv):
             not_green_count += 1
     total_lockdown = not_green_count > 10
 
-    print(f'number of districts: {len(districts_class.keys())}')
+    print(f'Number of districts: {len(districts_class.keys())}')
     print(f"Number of not green districts: {not_green_count}")
     print(f"Will a lockdown be forced on whole of Italy?: {'yes' if total_lockdown else 'no'}")
 
